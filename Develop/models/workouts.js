c37c1d6bcfd5a8aa5adafef1147a67f0a0bf77da
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-02-09 18:40:58 -0800

    Updated Server/workout.js

---
+++ Develop/models/workouts.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [{
+        name: {
+            type: String
+        }, 
+        type: {
+            type: String
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }]
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+workoutSchema.virtual("totalDuration").get(function() {
+    console.log("line 41", this);
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ Develop/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const Workout = require("./models/workouts");
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+app.post("/api/workouts", ({
+    body
+}, res) => {
+    Workout.create({day: new Date()}).then((data) => res.json(data)).catch(e => console.error(e))
+});
+
+app.get("/api/workouts", (req, res) => {
+    Workout.find({}, (error, data) => {
+        if (error) {
+            res.send(error);
+        } else {
+            console.log(data);
+            res.json(data);
+        }
+    });
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    Workout.find().limit(7)
+    .then(workout => res.json(workout))
+    .catch(e => console.error(e))
+
+    console.log("req.body= ",req.body);
+    console.log("res.json= ",res.json(workout));
+})
+
+
+app.put("/api/workouts/:id", (req, res) => {
+    console.log("line56 req.body= ",req.body);
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: req.body
+        }
+    }, {
+        new: true,
+        runValidators:true
+    })
+    .then(() => res.sendStatus(200))
+    .catch(e => console.error(e))
+    
+});
+
+app.listen(PORT, function() {
+    console.log(`The application is running on localhost:${PORT}`);
+})
