8237a00f9eae1c6f631e0288ff2bcc9beef8a76c
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-02-16 15:34:20 -0800

    Added api workout post

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,2 +1,14 @@
 const db = require("../models/workout.js");
 const mongoose = require ("mongoose");
+
+module.exports = (app) => {
+    
+    app.post("/api/workouts",(req, res) => {
+        db.create({
+            day: new Date(),
+        })
+        .then((data) => res.json(data))
+        .catch((err) => {
+            console.log("api/workouts error: ", err);
+        });
+    }); 
