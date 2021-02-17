51a42e6b6859480b0647c8d8c78353f257f8f4d2
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-02-16 16:09:17 -0800

    Updated server

--- Develop/server.js
+++ Develop/server.js
@@ -13,63 +13,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static("public"));
 
+htmlRoutes(app);
+apiRoutes(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
-
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
-});
-
-app.post("/api/workouts", ({
-    body
-}, res) => {
-    Workout.create({day: new Date()}).then((data) => res.json(data)).catch(e => console.error(e))
-});
-
-app.get("/api/workouts", (req, res) => {
-    Workout.find({}, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            console.log(data);
-            res.json(data);
-        }
-    });
-})
-
-app.get("/api/workouts/range", (req, res) => {
-    Workout.find().limit(7)
-    .then(workout => res.json(workout))
-    .catch(e => console.error(e))
-
-    console.log("req.body= ",req.body);
-    console.log("res.json= ",res.json(workout));
-})
-
-
-app.put("/api/workouts/:id", (req, res) => {
-    console.log("line56 req.body= ",req.body);
-    Workout.findByIdAndUpdate(req.params.id, {
-        $push: {
-            exercises: req.body
-        }
-    }, {
-        new: true,
-        runValidators:true
-    })
-    .then(() => res.sendStatus(200))
-    .catch(e => console.error(e))
-    
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 app.listen(PORT, function() {
